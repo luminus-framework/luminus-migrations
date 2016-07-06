ed3e9bb52a90b52160289ed3fc7a1e593d6b03fe
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-07-06 17:56:50 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus-migrations "0.2.3"
+(defproject luminus-migrations "0.2.4"
   :description "Luminus migrations"
   :url "https://github.com/luminus-framework/luminus-migrations"
   :license {:name "Eclipse Public License"
