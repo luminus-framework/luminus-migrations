14708cb424a372bcdbb45856902258cb9154382b
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2018-10-25 20:12:11 -0400

    bumped up migratus

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject luminus-migrations "0.5.9"
+(defproject luminus-migrations "0.6.0"
   :description "Luminus migrations"
   :url "https://github.com/luminus-framework/luminus-migrations"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [migratus "1.1.5"]
+                 [migratus "1.1.6"]
                  [com.carouselapps/to-jdbc-uri "0.5.0"]])
